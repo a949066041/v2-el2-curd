7ab71340cdb549548673c94707c09e8595d1e7cf
mac-wangsh <46606405@qq.com>
2021-09-21 15:34:43 +0800

    build release

--- components/TipBlock/index.jsx
+++ components/TipBlock/index.jsx
@@ -2,9 +2,32 @@ import './style.scss'
 
 export default {
   name: 'TipBlock',
-  render() {
+  functional: true,
+  props: {
+    tipStyle: {
+      type: Object,
+      default: () => ({})
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    bottom: {
+      type: Number,
+      default: 16
+    },
+    size: {
+      type: String,
+      default: '16px'
+    }
+  },
+  render(h, c) {
     return (
-      <div class='tip-block'>123</div>
+      <div class='mx-tip-block' pl10 style={{
+        fontSize: c.props.size,
+        marginBottom: `${c.props.bottom}px`,
+        ...c.props.tipStyle
+      }}>{ c.props.title }</div>
     )
   }
 }
